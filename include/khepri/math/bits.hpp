f7245b46aacaa0bfb9e63190f7766f21536530f4
Mike Lankamp <mike.lankamp@gmail.com>
2021-02-17 22:26:07 +0100

    feat: add `ceil_power_of_two` method to round up to the next power of two

--- include/khepri/math/bits.hpp
+++ include/khepri/math/bits.hpp
@@ -44,4 +44,24 @@ inline unsigned int bitcount(std::uint64_t value)
 #endif
 }
 
+/// Returns the first power of two equal to or greater than the value
+inline constexpr std::uint32_t ceil_power_of_two(std::uint32_t value)
+{
+    // From Bit Twiddling Hacks:
+    // "It works by copying the highest set bit to all of the lower bits, and then adding one,
+    //  which results in carries that set all of the lower bits to 0 and one bit beyond the highest
+    //  set bit to 1. If the original number was a power of 2, then the decrement will reduce it to
+    //  one less, so that we round up to the same original value."
+    //
+    // Using a bitscan instrinsic might be an alternative, but that may take several cycles so some
+    // performance measurement would have to prove it's worth it.
+    value--;
+    value |= value >> 1;
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    return value + 1;
+}
+
 } // namespace khepri
