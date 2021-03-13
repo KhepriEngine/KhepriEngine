4f8f2717cebc8ff4d5020fe9a9ccf7de71d037e9
Mike Lankamp <mike.lankamp@gmail.com>
2021-03-13 16:37:07 +0100

    feat: add method to test if a point is inside a rectangle

--- include/khepri/math/rect.hpp
+++ include/khepri/math/rect.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "point.hpp"
+
 namespace khepri {
 
 /**
@@ -19,5 +21,20 @@ struct Rect
     /// The height of the rectangle
     unsigned long height;
 };
+
+/**
+ * Determines if a point is inside a rectangle.
+ *
+ * \param[in] p the point
+ * \param[in] r the rectangle
+ *
+ * The rectangle's width and height are exclusive: the positions described by x + width or y +
+ * height are considered to be outside of the rectangle.
+ */
+inline constexpr bool inside(const Point& p, const Rect& r)
+{
+    return p.x >= r.x && p.y >= r.y && static_cast<unsigned long>(p.x - r.x) < r.width &&
+           static_cast<unsigned long>(p.y - r.y) < r.height;
+}
 
 } // namespace khepri
