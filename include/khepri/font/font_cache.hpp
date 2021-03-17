7cacb64832bb28b7b6e54b153c5ef65c153baa70
Mike Lankamp <mike.lankamp@gmail.com>
2021-03-17 15:45:10 +0100

    feat: add font cache
    A font cache caches fonts to avoid wasting resources on duplicate fonts with identical options.

---
+++ include/khepri/font/font_cache.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "font.hpp"
+#include "font_face_desc.hpp"
+#include "font_options.hpp"
+
+#include <khepri/utility/string.hpp>
+
+#include <map>
+#include <memory>
+#include <string_view>
+
+namespace khepri::font {
+
+/**
+ * Creates and caches fonts.
+ *
+ * Use a \a FontCache to avoid duplicate instantiations of similar fonts. Request a shared font
+ * reference with \ref FontCache::get.
+ */
+class FontCache final
+{
+public:
+    FontCache();
+    ~FontCache();
+
+    /// Constructs a font cache by moving from another font cache
+    FontCache(FontCache&& cache) noexcept;
+
+    /// Moves another font cache into this object
+    FontCache& operator=(FontCache&& cache) noexcept;
+
+    FontCache(const FontCache& cache) = delete;
+    FontCache& operator=(const FontCache& cache) = delete;
+
+    /**
+     * Adds a font face to the cache.
+     *
+     * \param[in] name the font face's name
+     * \param[in] font_face_desc the font face's description
+     *
+     * After adding, the face's name can be used in \ref FontCache::get to create fonts with.
+     */
+    void add_face(std::string_view name, const FontFaceDesc& font_face_desc);
+
+    /**
+     * Creates or retrieves a font for a given face with given options.
+     *
+     * \param[in] font_face_name the name of the font face to create the font from.
+     * \param[in] options the font options for the desired font.
+     *
+     * If the requested font already exists in the cache, it is not created again, but the
+     * previously created shared reference is returned.
+     */
+    std::shared_ptr<Font> get(std::string_view font_face_name, const FontOptions& options);
+
+    /**
+     * Clears the font cache.
+     *
+     * This removes all font faces and local references to previously created fonts.
+     * Previously issued font references remain valid.
+     */
+    void clear();
+
+private:
+    class FaceCache;
+
+    std::map<std::string, std::unique_ptr<FaceCache>, CaseInsensitiveLess> m_faces;
+};
+
+} // namespace khepri::font
---
+++ src/font/font_cache.cpp
@@ -0,0 +1,86 @@
+#include <khepri/exceptions.hpp>
+#include <khepri/font/font_cache.hpp>
+#include <khepri/font/font_face.hpp>
+
+#include <map>
+#include <tuple>
+
+namespace khepri {
+
+bool operator<(const ColorRGB& c1, const ColorRGB& c2) noexcept
+{
+    return std::tie(c1.r, c1.g, c1.b) < std::tie(c2.r, c2.g, c2.b);
+}
+
+namespace font {
+
+bool operator<(const FontOptions& options1, const FontOptions& options2) noexcept
+{
+    return std::tie(options1.font_size_px, options1.color_top, options1.color_bottom,
+                    options1.vert_scale, options1.stroke_size_px, options1.stroke_color,
+                    options1.embossed) < std::tie(options2.font_size_px, options2.color_top,
+                                                  options2.color_bottom, options2.vert_scale,
+                                                  options2.stroke_size_px, options2.stroke_color,
+                                                  options2.embossed);
+}
+
+class FontCache::FaceCache
+{
+public:
+    explicit FaceCache(const FontFaceDesc& font_face_desc) : m_face(font_face_desc) {}
+
+    std::shared_ptr<Font> get(const FontOptions& options)
+    {
+        auto it = m_fonts.find(options);
+        if (it != m_fonts.end()) {
+            return it->second;
+        }
+        std::shared_ptr<Font> font = m_face.create_font(options);
+        m_fonts.insert({options, font});
+        return font;
+    }
+
+    void clear()
+    {
+        m_fonts.clear();
+    }
+
+private:
+    FontFace m_face;
+
+    std::map<FontOptions, std::shared_ptr<Font>> m_fonts;
+};
+
+FontCache::FontCache()  = default;
+FontCache::~FontCache() = default;
+
+FontCache::FontCache(FontCache&& cache) noexcept = default;
+FontCache& FontCache::operator=(FontCache&& cache) noexcept = default;
+
+void FontCache::add_face(std::string_view name, const FontFaceDesc& font_face_desc)
+{
+    const auto it = m_faces.find(name);
+    if (it != m_faces.end()) {
+        throw ArgumentError();
+    }
+    m_faces.emplace(name, std::make_unique<FaceCache>(font_face_desc));
+}
+
+std::shared_ptr<Font> FontCache::get(std::string_view font_face_name, const FontOptions& options)
+{
+    const auto it = m_faces.find(font_face_name);
+    if (it == m_faces.end()) {
+        return nullptr;
+    }
+    return it->second->get(options);
+}
+
+void FontCache::clear()
+{
+    for (const auto& pair : m_faces) {
+        pair.second->clear();
+    }
+}
+
+} // namespace font
+} // namespace khepri
