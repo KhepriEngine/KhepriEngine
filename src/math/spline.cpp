325582174f2e9f7566e5b00cb0294551a56650b7
Mike Lankamp <mike.lankamp@gmail.com>
2022-07-22 23:58:27 +0200

    feat: add CubicSpline class
    The CubicSpline class calculates a smooth curve ("spline") through a series of control points by piecewise interpolation with third-order ("cubic") polynomials.

---
+++ src/math/spline.cpp
@@ -0,0 +1,276 @@
+#include <khepri/exceptions.hpp>
+#include <khepri/math/math.hpp>
+#include <khepri/math/spline.hpp>
+#include <khepri/utility/functional.hpp>
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iterator>
+
+namespace khepri {
+
+float CubicSpline::Polynomial::sample(float x) const noexcept
+{
+    assert(x >= 0 && x <= 1);
+    return a + (b + (c + d * x) * x) * x;
+}
+
+std::vector<CubicSpline::Polynomial>
+CubicSpline::Polynomials::calculate_polynomials(gsl::span<const float> points)
+{
+    if (points.size() < 2) {
+        throw khepri::ArgumentError();
+    }
+
+    if (points.size() == 2) {
+        // Straight line of (0,points[0]) to (1,points[1])
+        return {{points[0], points[1] - points[0], 0, 0}};
+    }
+
+    /*
+     * Define N cubic polynomials (fᵢ(x) for 0 ≤ i < N) that interpolate the N+1 points, where
+     * points[i] is assumed to be the value for xᵢ, with x₀ < x₁ < ... < xₙ (i.e. x is
+     * incrementing), with their first and second derivatives:
+     *
+     *   fᵢ(x)   = Aᵢ + Bᵢ·(x-xᵢ) + Cᵢ·(x-xᵢ)² + Dᵢ·(x-xᵢ)³                (1)
+     *   f′ᵢ(x)  = Bᵢ + 2·Cᵢ·(x-xᵢ) + 3·Dᵢ·(x-xᵢ)²                         (2)
+     *   f′′ᵢ(x) = 2·Cᵢ + 6·Dᵢ·(x-xᵢ)                                      (3)
+     *
+     * where fᵢ is valid for the domain [xᵢ, xᵢ₊₁]. To solve the 4·N coefficients of these
+     * polynomials, we need 4·N equations. We start by constraining f to interpolate `points` (eq.
+     * 1) and be C² continuous on every point (position, tangent and curvature match; eqs. 2–4):
+     *
+     *   fᵢ(xᵢ)     = points[i]     for 0 ≤ i < N                          (4)
+     *   fᵢ(xᵢ₊₁)   = fᵢ₊₁(xᵢ₊₁)    for 0 ≤ i < N                          (5)
+     *   f′ᵢ(xᵢ₊₁)  = f′ᵢ₊₁(xᵢ₊₁)   for 0 ≤ i < N-1                        (6)
+     *   f′′ᵢ(xᵢ₊₁) = f′′ᵢ₊₁(xᵢ₊₁)  for 0 ≤ i < N-1                        (7)
+     *
+     * This yields 4·N-2 equations. We also use _natural_ cubic splines: the end points have zero
+     * curvature. This gives us 2 additional equations for the total 4·N:
+     *
+     *   f′′₀ (x₀) = 0                                                     (8)
+     *   f′′ₙ₋₁(xₙ) = 0                                                     (9)
+     *
+     * Solving Aᵢ is trivial: eq. 4 simplifies to Aᵢ = points[i].
+     * From eqs. 2–3, it follows that:
+     *
+     *   f′ᵢ(xᵢ)  =   Bᵢ
+     *   f′′ᵢ(xᵢ) = 2·Cᵢ
+     *
+     * Let xᵢ₊₁ - xᵢ = 1 (i.e. assume the points are uniformly sampled at x = [0,1, ..., N]) and
+     * substitute eqs. 1–3 into eqs 5–7:
+     *
+     *   Aᵢ + Bᵢ +   Cᵢ +   Dᵢ = Aᵢ₊₁                                      (10)
+     *        Bᵢ + 2·Cᵢ + 3·Dᵢ = Bᵢ₊₁                                      (11)
+     *               Cᵢ + 3·Dᵢ = Cᵢ₊₁                                      (12)
+     *
+     * Rewrite eq. 12 for Dᵢ and substitute into eqs. 10 and 11:
+     *
+     *                   Dᵢ = ⅓·(Cᵢ₊₁ - Cᵢ)                                (13)
+     *   Bᵢ + ⅓·Cᵢ₊₁ + ⅔·Cᵢ =    Aᵢ₊₁ - Aᵢ                                 (14)
+     *          Cᵢ₊₁ +   Cᵢ =    Bᵢ₊₁ - Bᵢ                                 (15)
+     *
+     * Rewrite eq. 14 for Bᵢ and Bᵢ₊₁:
+     *
+     *   Bᵢ   = Aᵢ₊₁ - Aᵢ   - ⅓·(Cᵢ₊₁ + 2·Cᵢ  )                            (16)
+     *   Bᵢ₊₁ = Aᵢ₊₂ - Aᵢ₊₁ - ⅓·(Cᵢ₊₂ + 2·Cᵢ₊₁)                            (17)
+     *
+     * Substitute eqs. 16 and 17 into eq. 15 and rewrite into:
+     *
+     *   Cᵢ + 4·Cᵢ₊₁ + Cᵢ₊₂ = 3·(Aᵢ - 2·Aᵢ₊₁ + Aᵢ₊₂)    for 0 ≤ i ≤ N-2    (18)
+     *
+     * Shift the index downward by one to get
+     *
+     *   Cᵢ₋₁ + 4·Cᵢ + Cᵢ₊₁ = 3·(Aᵢ₋₁ - 2·Aᵢ + Aᵢ₊₁)    for 1 ≤ i ≤ N-1    (19)
+     *
+     * Note that A is known. From eqs. 2, 3, 8 and 9 we additionally get:
+     *
+     *   C₀  = 0                                                           (20)
+     *   Cₙ₋₁ = 0                                                           (21)
+     *
+     * Eqs. 19–21 describe N+1 linear equations that can be expressed in matrix form:
+     *
+     *   [1 0 0 0 0 0]  [C₀ ]   [          0          ]
+     *   [1 4 1 0 0 0]  [C₁ ]   [  3·(A₀ - 2·A₁ + A₂) ]
+     *   [0 1 4 1 0 0]  [C₂ ]   [  3·(A₁ - 2·A₂ + A₃) ]
+     *   [    ...    ]  [...] = [          ...        ]
+     *   [0 0 0 1 4 1]  [Cₙ₋₁]   [3·(Aₙ₋₂ - 2·Aₙ₋₁ + Aₙ)]
+     *   [0 0 0 0 0 1]  [Cₙ  ]   [           0         ]
+     *
+     * Or M · C = A, where the matrix M and vector A are known and C is the vector of Cᵢ components.
+     * Solving for C becomes C = M⁻¹ · A. M is a diagonally dominant tridiagonal matrix so
+     * calculating M⁻¹ can be done via Thomas' algorithm.
+     *
+     * With A and C known, eqs. 10 and 12 can be used to find D and B.
+     */
+
+    // Run Thomas' algorithm
+
+    // The new superdiagonal elements
+    std::vector<float> superd(points.size() - 1);
+
+    // The result elements of the expression (the knowns).
+    // This will also hold the computed unknowns during the backpropagation step.
+    std::vector<float> result(points.size());
+
+    superd.front() = result.front() = 0;
+    for (std::size_t i = 1; i < superd.size(); ++i) {
+        float result_i = 3 * (points[i - 1] - 2 * points[i] + points[i + 1]);
+        superd[i]      = 1.0 / (4.0f - superd[i - 1]);
+        result[i]      = (result_i - result[i - 1]) * superd[i];
+    }
+
+    // Back-substitute to find the unknown vector with coefficient C for the polynomials
+    result.back() = 0;
+    for (std::size_t i = result.size() - 1; i > 0; --i) {
+        result[i - 1] -= superd[i - 1] * result[i];
+    }
+
+    // Construct the polynomials from the coefficients a, b, c and d.
+    std::vector<CubicSpline::Polynomial> polynomials(points.size() - 1);
+    for (std::size_t i = 0; i < polynomials.size(); ++i) {
+        const float a  = points[i];
+        const float d  = (result[i + 1] - result[i]) / 3.0;
+        const float b  = points[i + 1] - points[i] - result[i] - d;
+        polynomials[i] = {a, b, result[i], d};
+    }
+    return polynomials;
+}
+
+CubicSpline::Polynomials::Polynomials(gsl::span<const Vector3> points)
+    : m_polynomials_x(calculate_polynomials(pluck(points, &Vector3::x)))
+    , m_polynomials_y(calculate_polynomials(pluck(points, &Vector3::y)))
+    , m_polynomials_z(calculate_polynomials(pluck(points, &Vector3::z)))
+{}
+
+Vector3 CubicSpline::Polynomials::sample(std::size_t index, float u) const noexcept
+{
+    assert(m_polynomials_x.size() == m_polynomials_y.size());
+    assert(m_polynomials_y.size() == m_polynomials_z.size());
+    assert(index < m_polynomials_x.size());
+    assert(u >= 0 && u <= 1);
+
+    const auto x = m_polynomials_x[index].sample(u);
+    const auto y = m_polynomials_y[index].sample(u);
+    const auto z = m_polynomials_z[index].sample(u);
+    return {x, y, z};
+}
+
+CubicSpline::CubicSpline(gsl::span<const Vector3> points)
+    : m_polynomials(points)
+    , m_arc_offsets(calculate_arc_offsets(m_polynomials))
+    , m_points{points.begin(), points.end()}
+{}
+
+float CubicSpline::arc_length(const Polynomials& polynomials, std::size_t index, float u_from,
+                              float u_to) noexcept
+{
+    // This method finds the arc length by recursive division:
+    // We divide the curve in two (half-way between u_from and u_to) and get the
+    //
+    assert(u_from <= u_to);
+    assert(u_from >= 0.0 && u_to <= 1.0);
+
+    constexpr auto min_accuracy = 0.00001f;
+
+    // Sample the cubic at the two end points
+    const auto v_from = polynomials.sample(index, u_from);
+    const auto v_to   = polynomials.sample(index, u_to);
+
+    // Get the chordal length of the curve between the two endpoints
+    const auto length = khepri::distance(v_from, v_to);
+    if (length < min_accuracy) {
+        // The length is too small, don't bother subdividing
+        return length;
+    }
+
+    // Find and sample the mid-way point
+    const auto u_mid = (u_from + u_to) / 2;
+    const auto v_mid = polynomials.sample(index, u_mid);
+
+    // Get the 'better' (more accurate) length by adding the chordal distances from the end points
+    // to the mid-way point. This is typically larger than the direct length (because it's a curve).
+    const auto better_length = khepri::distance(v_from, v_mid) + khepri::distance(v_mid, v_to);
+
+    // Calculate relative and absolute error
+    const auto rel_error = std::abs(length / better_length - 1);
+    const auto abs_error = std::abs(length - better_length);
+
+    if (rel_error < min_accuracy || abs_error < min_accuracy) {
+        // Both errors are acceptably small, stop recursing
+        return better_length;
+    }
+
+    // Recurse to add the arc lengths
+    return arc_length(polynomials, index, u_from, u_mid) +
+           arc_length(polynomials, index, u_mid, u_to);
+}
+
+std::vector<float> CubicSpline::calculate_arc_offsets(const Polynomials& polynomials) noexcept
+{
+    std::vector<float> arc_offsets(polynomials.size());
+    arc_offsets[0] = arc_length(polynomials, 0, 0.0f, 1.0f);
+    for (std::size_t i = 1; i < arc_offsets.size(); ++i) {
+        arc_offsets[i] = arc_offsets[i - 1] + arc_length(polynomials, i, 0.0f, 1.0f);
+    }
+    return arc_offsets;
+}
+
+[[nodiscard]] float CubicSpline::length_at(std::size_t point_index) const noexcept
+{
+    assert(point_index <= m_arc_offsets.size());
+    if (point_index == 0) {
+        return 0.0f;
+    }
+    return m_arc_offsets[point_index - 1];
+}
+
+Vector3 CubicSpline::sample(float t) const noexcept
+{
+    t = clamp(t, 0.0f, 1.0f);
+
+    // t is in arc length, find the segment that belongs to it
+    const auto spline_length = m_arc_offsets.back();
+    const auto arc_offset    = t * spline_length;
+    const auto index         = std::min<std::size_t>(
+        std::distance(m_arc_offsets.begin(),
+                      std::upper_bound(m_arc_offsets.begin(), m_arc_offsets.end(), arc_offset)),
+        m_arc_offsets.size() - 1);
+
+    auto arc_offset_segment = (index > 0 ? m_arc_offsets[index - 1] : 0.0f);
+
+    // Now find the uniform parameter for that segment
+    auto u_start          = 0.0f;
+    auto arc_offset_start = arc_offset_segment;
+    auto u_end            = 1.0f;
+    auto arc_offset_end   = m_arc_offsets[index];
+
+    // Bound the iterations so we don't end up in an infinite loop due to some floating point
+    // rounding oddity.
+    constexpr int max_iterations = 100;
+    float         u;
+    for (int i = 0; i < max_iterations; ++i) {
+        // Don't use midway interpolation, but linear interpolation based on the desired arc offset.
+        // This has a higher chance of `u` already being in the right area and thus needing fewer
+        // iterations.
+        const auto frac   = (arc_offset - arc_offset_start) / (arc_offset_end - arc_offset_start);
+        u                 = lerp(u_start, u_end, frac);
+        const auto length = arc_length(m_polynomials, index, 0.0, u) + arc_offset_segment;
+        if (std::abs(length - arc_offset) < 0.000001f) {
+            // We're close enough to the desired arc offset
+            break;
+        }
+        if (length < arc_offset) {
+            u_start          = u;
+            arc_offset_start = length;
+        } else {
+            u_end          = u;
+            arc_offset_end = length;
+        }
+    }
+
+    return m_polynomials.sample(index, u);
+}
+
+} // namespace khepri
---
+++ tests/cubic_spline_test.cpp
@@ -0,0 +1,272 @@
+#include <khepri/exceptions.hpp>
+#include <khepri/math/spline.hpp>
+#include <khepri/utility/string.hpp>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <cmath>
+
+using khepri::CubicSpline;
+using khepri::Vector3;
+using testing::TestWithParam;
+using testing::Values;
+
+namespace khepri {
+std::ostream& operator<<(std::ostream& os, const Vector3& v)
+{
+    return os << "{" << v.x << "," << v.y << "," << v.z << "}";
+}
+
+std::ostream& operator<<(std::ostream& os, const CubicSpline& c)
+{
+    return os << "spline through [" << khepri::join(c.points(), ", ") << "]";
+}
+
+[[nodiscard]] bool near(const Vector3& lhs, const Vector3& rhs, float abs_error)
+{
+    const auto& near = [&](float lhs, float rhs) { return std::abs(lhs - rhs) <= abs_error; };
+
+    return near(lhs.x, rhs.x) && near(lhs.y, rhs.y) && near(lhs.z, rhs.z);
+}
+
+} // namespace khepri
+
+namespace {
+
+auto Near(const Vector3& rhs, float abs_error = 0.001f)
+{
+    class Vector3NearMatcher
+    {
+    public:
+        using is_gtest_matcher = void;
+
+        explicit Vector3NearMatcher(const Vector3& rhs, float abs_error)
+            : m_rhs(rhs), m_abs_error(abs_error)
+        {}
+
+        bool MatchAndExplain(const Vector3& lhs, std::ostream*) const
+        {
+            return near(lhs, m_rhs, m_abs_error);
+        }
+
+        void DescribeTo(std::ostream* os) const
+        {
+            *os << "is near " << m_rhs;
+        }
+
+        void DescribeNegationTo(std::ostream* os) const
+        {
+            *os << "isn't near " << m_rhs;
+        }
+
+    private:
+        const Vector3 m_rhs;
+        float         m_abs_error;
+    };
+
+    return Vector3NearMatcher(rhs, abs_error);
+}
+
+// Performs basic checks on the spline such as interpolation of the configured points and C2
+// continuity.
+auto IsValidSpline()
+{
+    static constexpr auto MAX_ERROR = 0.0001f;
+
+    class ValidSplineMatcher
+    {
+    public:
+        using is_gtest_matcher = void;
+
+        bool MatchAndExplain(const CubicSpline& spline, std::ostream* os) const
+        {
+            if (!IsSplineInterpolating(spline)) {
+                if (os != nullptr) {
+                    *os << "spline is not interpolating";
+                }
+                return false;
+            }
+
+            if (!IsSplineContinuousInPosition(spline)) {
+                if (os != nullptr) {
+                    *os << "spline is not continuous in position";
+                }
+                return false;
+            }
+
+            if (!IsSplineContinuousInTangent(spline)) {
+                if (os != nullptr) {
+                    *os << "spline is not continuous in tangent";
+                }
+                return false;
+            }
+
+            if (!IsSplineContinuousInCurvature(spline)) {
+                if (os != nullptr) {
+                    *os << "spline is not continuous in curvature";
+                }
+                return false;
+            }
+            return true;
+        }
+
+        void DescribeTo(std::ostream* os) const
+        {
+            *os << "is a valid spline";
+        }
+
+        void DescribeNegationTo(std::ostream* os) const
+        {
+            *os << "is not a valid spline";
+        }
+
+    private:
+        static bool IsSplineInterpolating(const CubicSpline& spline) noexcept
+        {
+            const auto& points = spline.points();
+            for (std::size_t i = 0; i < points.size(); ++i) {
+                const float t     = spline.length_at(i) / spline.length();
+                const auto  value = spline.sample(t);
+                if (!near(value, points[i], MAX_ERROR)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool IsSplineContinuousInPosition(const CubicSpline& spline) noexcept
+        {
+            // Check both sides of the points to see if the positions are really close to each other
+            constexpr auto offset = std::numeric_limits<float>::epsilon();
+            const auto&    points = spline.points();
+            for (std::size_t i = 1; i < points.size() - 1; ++i) {
+                const auto t = spline.length_at(i) / spline.length();
+                ;
+
+                const auto pos_left  = spline.sample(t - offset);
+                const auto pos_right = spline.sample(t + offset);
+                if (!near(pos_left, pos_right, MAX_ERROR)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool IsSplineContinuousInTangent(const CubicSpline& spline) noexcept
+        {
+            // Check both sides of the points to see if the tangents are really close to each other
+            constexpr auto offset = std::numeric_limits<float>::epsilon();
+            const auto&    points = spline.points();
+            for (std::size_t i = 1; i < points.size() - 1; ++i) {
+                const auto t = spline.length_at(i) / spline.length();
+                ;
+
+                // Note: technically we should divide by @a offset to get the tangent, but since
+                // that's the same on both side of the comparison, we can ignore it.
+                const auto tangent_left  = spline.sample(t) - spline.sample(t - offset);
+                const auto tangent_right = spline.sample(t + offset) - spline.sample(t);
+                if (!near(tangent_left, tangent_right, MAX_ERROR)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static bool IsSplineContinuousInCurvature(const CubicSpline& spline) noexcept
+        {
+            // Check both sides of the points to see if the curvatures are really close to each
+            // other
+            constexpr auto offset = 0.0001f;
+            const auto&    points = spline.points();
+            for (std::size_t i = 1; i < points.size() - 1; ++i) {
+                const auto t = static_cast<float>(i) / (points.size() - 1);
+
+                // Note: technically we should divide by @a offset to get the tangents, but since
+                // that's the same on both side of the comparison, we can ignore it. Same for
+                // curvature
+                const auto tangent_left_1  = spline.sample(t) - spline.sample(t - offset);
+                const auto tangent_right_1 = spline.sample(t + offset) - spline.sample(t);
+
+                const auto tangent_left_2 =
+                    spline.sample(t - offset) - spline.sample(t - 2 * offset);
+                const auto tangent_right_2 =
+                    spline.sample(t + 2 * offset) - spline.sample(t + offset);
+
+                const auto curvature_left  = tangent_left_1 - tangent_left_2;
+                const auto curvature_right = tangent_right_2 - tangent_right_1;
+
+                if (!near(curvature_left, curvature_right, MAX_ERROR)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    };
+
+    return ValidSplineMatcher{};
+}
+
+} // namespace
+
+TEST(CubicSplineTest, SplineWithZeroPoints_ThrowsArgumentError)
+{
+    EXPECT_THROW(CubicSpline{{}}, khepri::ArgumentError);
+}
+
+TEST(CubicSplineTest, SplineWithOnePoint_ThrowsArgumentError)
+{
+    static constexpr Vector3 points[] = {{0, 0, 0}};
+    EXPECT_THROW(CubicSpline{points}, khepri::ArgumentError);
+}
+
+TEST(CubicSplineTest, LineSpline_HasCorrectLength)
+{
+    static constexpr Vector3 points[] = {{0, 0, 0}, {1, 1, 1}};
+    EXPECT_NEAR(CubicSpline{points}.length(), std::sqrt(3.0f), 0.00000001f);
+}
+
+class ValidCubicSplineTest : public testing::TestWithParam<std::vector<Vector3>>
+{};
+
+// Test that the spline is valid (interpolating & continuous)
+TEST_P(ValidCubicSplineTest, SplineIsValid)
+{
+    CubicSpline spline(GetParam());
+    EXPECT_THAT(spline, IsValidSpline());
+}
+
+// Test that when sampling at a regular interval along the spline, the distance between returned
+// points is roughly the same.
+TEST_P(ValidCubicSplineTest, SplinePointsAreEquidistant)
+{
+    static constexpr auto MAX_ERROR = 0.0001f;
+
+    CubicSpline spline(GetParam());
+
+    std::vector<Vector3> samples(201);
+    const auto           sample_length = spline.length() / (samples.size() - 1);
+    for (std::size_t i = 0; i < samples.size(); ++i) {
+        auto t     = static_cast<float>(i) / (samples.size() - 1);
+        samples[i] = spline.sample(t);
+    }
+
+    const float distance = khepri::distance(samples[1], samples[0]);
+    for (std::size_t i = 1; i < samples.size() - 1; ++i) {
+        EXPECT_NEAR(distance, sample_length, MAX_ERROR) << " for point " << i;
+    }
+}
+
+INSTANTIATE_TEST_CASE_P(ValidCubicSplineTest, ValidCubicSplineTest,
+                        Values(std::vector<Vector3>{{{0, 0, 0}, {1, 1, 1}}},
+                               std::vector<Vector3>{{{0, 0, 0}, {1, 1, 0}, {2, 0, 0}}},
+                               std::vector<Vector3>{{{0, 1, 0}, {2, 2, 0}, {5, 0, 0}, {8, 0, 0}}},
+                               std::vector<Vector3>{
+                                   {{5, -7, 0}, {2, 0, 0}, {1, 3, 0}, {2, 2, 0}, {5, -3, 0}}},
+                               std::vector<Vector3>{{{-1.5, -1.2, 0},
+                                                     {-0.2, 0, 0},
+                                                     {1, 0.5, 0},
+                                                     {5, 1, 0},
+                                                     {10, 1.2, 0},
+                                                     {15, 2, 0},
+                                                     {20, 1, 0}}}));
